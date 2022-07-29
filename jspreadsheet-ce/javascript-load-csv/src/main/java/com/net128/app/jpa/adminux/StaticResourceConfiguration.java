099243663a7b5ec089eca38370c0e9f699248ed3
michael <michael@mac>
2022-07-29 20:47:10 +0200

    ..

---
+++ jspreadsheet-ce/javascript-load-csv/src/main/java/com/net128/app/jpa/adminux/StaticResourceConfiguration.java
@@ -0,0 +1,38 @@
+package com.net128.app.jpa.adminux;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+import org.springframework.web.servlet.resource.ResourceResolverChain;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Configuration
+public class StaticResourceConfiguration implements WebMvcConfigurer {
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.setOrder(Ordered.LOWEST_PRECEDENCE)
+			.addResourceHandler("/**")
+			.addResourceLocations("classpath:/static/", "classpath:/public/")
+			.resourceChain(true)
+			.addResolver(new IndexFallbackResourceResolver());
+	}
+
+	private static class IndexFallbackResourceResolver extends PathResourceResolver {
+		public Resource resolveResourceInternal(HttpServletRequest request,
+				String requestPath, List<? extends Resource> locations,
+				ResourceResolverChain chain) {
+			var resource = super.resolveResourceInternal(
+				request, requestPath, locations, chain);
+			if (resource == null) {
+				resource = super.resolveResourceInternal(request,
+					requestPath+"index.html", locations, chain);
+			}
+			return resource;
+		}
+	}
+}
