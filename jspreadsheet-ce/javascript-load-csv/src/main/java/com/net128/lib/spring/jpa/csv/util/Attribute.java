3bb863c0b6272e87c45648649496ef38eef4dc45
michael <michael@mac>
2022-08-04 13:06:05 +0200

    flexbox

--- jspreadsheet-ce/javascript-load-csv/src/main/java/com/net128/lib/spring/jpa/csv/util/Attribute.java
+++ jspreadsheet-ce/javascript-load-csv/src/main/java/com/net128/lib/spring/jpa/csv/util/Attribute.java
@@ -4,7 +4,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.time.ZonedDateTime;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -22,14 +27,23 @@ public class Attribute {
 		name = attribute.getName();
 		var javaType = attribute.getJavaType();
 		if (Number.class.isAssignableFrom(javaType) || javaType.isPrimitive()) {
-			if (javaType.getName().toLowerCase()
-					.matches(".*(long|integer|short|byte).*"))
+			var typeName = javaType.getName().toLowerCase();
+			if(typeName.equals("char"))
+				type = AttributeType.String;
+			else if (typeName.matches(".*(long|integer|short|byte|int|boolean).*"))
 				type = AttributeType.Int;
 			else type = AttributeType.Float;
 		} else if (javaType.isEnum()) {
 			type = AttributeType.Enum;
 			enumConstants = Arrays.stream(javaType.getEnumConstants())
-					.map(Object::toString).collect(Collectors.toSet());
+				.map(Object::toString).collect(Collectors.toSet());
+		} else if (javaType.equals(Instant.class)
+				||javaType.equals(ZonedDateTime.class)
+				||javaType.equals(OffsetDateTime.class)
+			) {
+			type = AttributeType.DateTime;
+		} else if (javaType.equals(Date.class) || javaType.equals(LocalDate.class)) {
+			type = AttributeType.Date;
 		} else type = AttributeType.String;
 		hidden = Props.isHiddenField(attribute.getJavaMember().getDeclaringClass(), name);
 		log.info(name + ": " + attribute.getJavaType() + " -> " + type);
