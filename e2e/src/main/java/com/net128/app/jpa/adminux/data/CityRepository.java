9802f57bd1acb6230e3c099c76c1d40b457a9d63
michael <michael@mac>
2022-10-15 00:13:44 +0200

    mkae findall return results in desc order of population

--- e2e/src/main/java/com/net128/app/jpa/adminux/data/CityRepository.java
+++ e2e/src/main/java/com/net128/app/jpa/adminux/data/CityRepository.java
@@ -2,5 +2,9 @@ package com.net128.app.jpa.adminux.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CityRepository extends JpaRepository<City, Long> {
+	List<City> findAllByOrderByPopulationDesc();
+	default List<City> findAll() { return findAllByOrderByPopulationDesc(); }
 }
