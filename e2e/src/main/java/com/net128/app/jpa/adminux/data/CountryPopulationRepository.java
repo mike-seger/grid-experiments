3e7a4f80d8bdade9fb776d391bba8b05aa51e38f
michael <michael@mac>
2022-10-15 00:37:05 +0200

    make findall return results in desc order of population

--- e2e/src/main/java/com/net128/app/jpa/adminux/data/CountryPopulationRepository.java
+++ e2e/src/main/java/com/net128/app/jpa/adminux/data/CountryPopulationRepository.java
@@ -2,5 +2,9 @@ package com.net128.app.jpa.adminux.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CountryPopulationRepository extends JpaRepository<CountryPopulation, Long> {
+	List<CountryPopulation> findAllByOrderByPopulationDesc();
+	default List<CountryPopulation> findAll() { return findAllByOrderByPopulationDesc(); }
 }
